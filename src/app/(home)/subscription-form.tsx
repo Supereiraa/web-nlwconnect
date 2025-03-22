f47381e59226fdabe5ccfdfe67cadfc313b36d33
Supereiraa <suuppereira@gmail.com>
2025-03-22 11:35:31 -0300

    aula2

---
+++ src/app/(home)/subscription-form.tsx
@@ -0,0 +1,126 @@
+'use client'
+
+import { Button } from '@/components/button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight, Mail, MapIcon, PhoneCall, User } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, 'Digite seu nome completo'),
+  email: z.string().email('Digite um e-mail válido'),
+  city: z.string().min(2, 'Digite uma cidade válida'),
+  phone: z
+    .string()
+    .regex(/^\d{10,11}$/, 'Digite um número de telefone válido (com DDD)'),
+})
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>
+
+export function SubscriptionForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  })
+
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data)
+  }
+  return (
+    <form
+      onSubmit={handleSubmit(onSubscribe)}
+      className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
+    >
+      <h2 className="font-heading font-semibold text-gray-200 text-xl">
+        Inscrição
+      </h2>
+
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="Nome Completo"
+              {...register('name')}
+            />
+          </InputRoot>
+
+          {errors.name && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.name.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+            <InputField
+              type="email"
+              placeholder="Email"
+              {...register('email')}
+            />
+          </InputRoot>
+
+          {errors.email && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <PhoneCall />
+            </InputIcon>
+            <InputField
+              type="tel"
+              placeholder="Whatsapp"
+              {...register('phone')}
+            />
+          </InputRoot>
+
+          {errors.phone && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.phone.message}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <MapIcon />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="Cidade"
+              {...register('city')}
+            />
+          </InputRoot>
+
+          {errors.city && (
+            <p className="text-danger text-xs font-semibold">
+              {errors.city.message}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <Button type="submit">
+        Confirmar
+        <ArrowRight />
+      </Button>
+    </form>
+  )
+}
